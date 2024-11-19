0ef0b3f76a1d0f28a141502b88fe22ef31be4ff0
Hiran S <hirans2006@gmail.com>
2024-11-19 22:51:09 +0530

    Started

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View, TouchableOpacity } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Text style={{textAlign:"center", padding:20, backgroundColor:"#3385ff", color:"white",fontSize:18}}>Let' s get started</Text>
+    </View>
+  );
+}
