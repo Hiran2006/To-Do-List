0f80f0384157c15312e01b11ac43dc511ac79b86
Hiran S <hirans2006@gmail.com>
2024-11-20 19:55:59 +0530

    just update

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,12 @@ import { Text, View, TouchableOpacity } from "react-native";
 export default function Index() {
   return (
     <View>
-      <Text style={{textAlign:"center", padding:20, backgroundColor:"#3385ff", color:"white",fontSize:18}}>Let' s get started</Text>
+      <Text style={{textAlign:"center", padding:20, backgroundColor:"#3385ff", color:"white",fontSize:25}}>Let's get started</Text>
+      <Text style={{padding:30,fontSize:18}}>Available Tasks</Text>
+      <DoLists/>
     </View>
   );
+}
+function DoLists(){
+  return(<Text>HEllo</Text>)
 }
